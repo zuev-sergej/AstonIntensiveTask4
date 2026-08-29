ade3a2c5dcdf372d2b3399f2f9ab0f1d59ee4b16
Zuev_Sergey <zuevkajo@gmail.com>
2026-08-30 00:15:42 +0300

    add integration tests user-service

--- src/main/java/ru/aston/userservice/model/User.java
+++ src/main/java/ru/aston/userservice/model/User.java
@@ -1,6 +1,7 @@
 package ru.aston.userservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {

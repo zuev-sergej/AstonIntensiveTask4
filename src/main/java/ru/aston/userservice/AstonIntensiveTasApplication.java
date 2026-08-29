53282c35d6bc224afb23a8ab562d4749fb03b1df
Zuev_Sergey <zuevkajo@gmail.com>
2026-08-29 11:49:17 +0300

    Initial commit

---
+++ src/main/java/ru/aston/userservice/AstonIntensiveTasApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.userservice.astonintensivetasck4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonIntensiveTasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonIntensiveTasApplication.class, args);
+    }
+
+}
